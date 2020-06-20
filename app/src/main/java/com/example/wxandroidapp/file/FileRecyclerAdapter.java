97bc526709dff98bb04af22cd68174741e09c33f
FishInWater-1999 <814484626@qq.com>
2020-06-20 20:26:29 +0800

    ADD:「分享文件」

---
+++ app/src/main/java/com/example/wxandroidapp/file/FileRecyclerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.wxandroidapp.file;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wxandroidapp.music.MusicRecyclerAdapter;
+
+public class FileRecyclerAdapter extends RecyclerView.Adapter<FileRecyclerAdapter.FileViewHoulder>{
+
+
+    @NonNull
+    @Override
+    public FileRecyclerAdapter.FileViewHoulder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FileRecyclerAdapter.FileViewHoulder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class FileViewHoulder extends RecyclerView.ViewHolder {
+
+        public FileViewHoulder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/wxandroidapp/file/PDFDetailActivity.java
@@ -0,0 +1,130 @@
+
+//        displayFromFile1("https://staticzcjb.weibangong.com/pdf/business_license.pdf", "business_license.pdf");
+package com.example.wxandroidapp.file;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.wxandroidapp.R;
+
+public class PDFDetailActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private String fileUrl = "https://staticzcjb.weibangong.com/pdf/business_license.pdf";
+    private String  pdfHtml = "file:///android_asset/pdf.html";
+    private ProgressBar pro;
+    public static final int LOAD_JAVASCRIPT = 0X01;
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String javaScript = "javascript: getpdf2('"+ fileUrl +"')";
+            mWebView.loadUrl(javaScript);
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdfdetail);
+        initView();
+
+    }
+
+    private void initView() {
+
+        mWebView = (WebView) findViewById(R.id.web);
+        pro = (ProgressBar) findViewById(R.id.pro);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDomStorageEnabled(true);
+        mWebView.addJavascriptInterface(new JsObject(this, fileUrl), "client");
+
+
+
+        mWebView.loadUrl(pdfHtml);
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                pro.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                handler.sendEmptyMessage(LOAD_JAVASCRIPT);
+                Toast.makeText(PDFDetailActivity.this, "开始请求pdf文件", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    public void pre(View view) {
+        String javaScript = "javascript: getpdf2('"+ fileUrl +"')";
+        mWebView.loadUrl(javaScript);
+
+    }
+
+    public void next(View view) {
+        mWebView.loadUrl("javascript: goNext()");
+
+    }
+
+    public void before(View view) {
+        mWebView.loadUrl("javascript: goPrevious()");
+    }
+
+
+    class JsObject {
+        Activity mActivity;
+        String url ;
+        public JsObject(Activity activity,String url) {
+            mActivity = activity;
+            this.url= url;
+        }
+
+        //    测试方法
+        @JavascriptInterface
+        public String dismissProgress() {
+            pro.setVisibility(View.GONE);
+            return this.url;
+        }
+    }
+
+
+    public static void actionStart(Activity activity) {
+        Intent intent = new Intent(activity, PDFDetailActivity.class);
+        activity.startActivity(intent);
+    }
+}
