b321343f79f6e4accd57abe4caf863762017274e
FishInWater-1999 <814484626@qq.com>
2020-06-20 20:27:42 +0800

    ADD:「清明上河图」

---
+++ app/src/main/java/com/example/wxandroidapp/qingming/QingmingActivity.java
@@ -0,0 +1,16 @@
+package com.example.wxandroidapp.qingming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wxandroidapp.R;
+
+public class QingmingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qingming);
+    }
+}
