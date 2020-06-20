01e4fe4f59a2cd286376dfb060fb2fc2868cc28e
FishInWater-1999 <814484626@qq.com>
2020-06-20 20:26:43 +0800

    ADD:「弹珠游戏」

---
+++ app/src/main/java/com/example/wxandroidapp/game/GameActivity.java
@@ -0,0 +1,16 @@
+package com.example.wxandroidapp.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wxandroidapp.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
