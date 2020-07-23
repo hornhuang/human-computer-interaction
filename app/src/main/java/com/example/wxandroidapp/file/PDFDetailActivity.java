dc50ccdc9934a2095bdfeccd8cdea02322950e7a
hornhuang <307975180@qq.com>
2020-07-23 15:23:55 +0800

    UPD：「 文件分享 - PDF 」

--- app/src/main/java/com/example/wxandroidapp/file/PDFDetailActivity.java
+++ app/src/main/java/com/example/wxandroidapp/file/PDFDetailActivity.java
@@ -1,19 +1,14 @@
-
-//        displayFromFile1("https://staticzcjb.weibangong.com/pdf/business_license.pdf", "business_license.pdf");
 package com.example.wxandroidapp.file;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -21,107 +16,44 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.wxandroidapp.R;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 
-public class PDFDetailActivity extends AppCompatActivity {
+public class PDFDetailActivity extends AppCompatActivity implements OnLoadCompleteListener, OnPageChangeListener {
 
-    private WebView mWebView;
-    private String fileUrl = "https://staticzcjb.weibangong.com/pdf/business_license.pdf";
-    private String  pdfHtml = "file:///android_asset/pdf.html";
-    private ProgressBar pro;
-    public static final int LOAD_JAVASCRIPT = 0X01;
-
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            String javaScript = "javascript: getpdf2('"+ fileUrl +"')";
-            mWebView.loadUrl(javaScript);
-        }
-    };
+    private PDFView pdfView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdfdetail);
-        initView();
-
+        initPDFView();
     }
 
-    private void initView() {
-
-        mWebView = (WebView) findViewById(R.id.web);
-        pro = (ProgressBar) findViewById(R.id.pro);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-        webSettings.setLoadsImagesAutomatically(true);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setSupportZoom(true);
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setDomStorageEnabled(true);
-        mWebView.addJavascriptInterface(new JsObject(this, fileUrl), "client");
-
-
-
-        mWebView.loadUrl(pdfHtml);
-
-        mWebView.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                pro.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                handler.sendEmptyMessage(LOAD_JAVASCRIPT);
-                Toast.makeText(PDFDetailActivity.this, "开始请求pdf文件", Toast.LENGTH_SHORT).show();
-            }
-
-        });
+    private void initPDFView() {
+        pdfView = (PDFView) findViewById(R.id.pdfView);
+        pdfView.fromAsset("think.pdf")
+                .defaultPage(0)
+                .onPageChange(PDFDetailActivity.this)
+                .enableAnnotationRendering(true)
+                .onLoad(PDFDetailActivity.this)
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .load();
     }
 
-    public void pre(View view) {
-        String javaScript = "javascript: getpdf2('"+ fileUrl +"')";
-        mWebView.loadUrl(javaScript);
-
+    @Override
+    public void loadComplete(int nbPages) {
+        Toast.makeText(this, "欢迎阅读《思考·快与慢》", Toast.LENGTH_SHORT).show();
     }
 
-    public void next(View view) {
-        mWebView.loadUrl("javascript: goNext()");
-
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        getSupportActionBar().setTitle("思考·快与慢 ");
+        getSupportActionBar().setSubtitle("当前进度： " + page + "/" + pageCount);
     }
-
-    public void before(View view) {
-        mWebView.loadUrl("javascript: goPrevious()");
-    }
-
-
-    class JsObject {
-        Activity mActivity;
-        String url ;
-        public JsObject(Activity activity,String url) {
-            mActivity = activity;
-            this.url= url;
-        }
-
-        //    测试方法
-        @JavascriptInterface
-        public String dismissProgress() {
-            pro.setVisibility(View.GONE);
-            return this.url;
-        }
-    }
-
 
     public static void actionStart(Activity activity) {
         Intent intent = new Intent(activity, PDFDetailActivity.class);
